58d4c15d9acc113eee9256bf72adfce9cbc76939
yogeshhere1999@gmail.com <yogeshhere1999@gmail.com>
2021-12-27 23:36:46 +0530

    added stickyNotes UI

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ Menubtn.addEventListener("click",()=>{
     
     
 })
-let toolPenFlag = false;
+let toolPenFlag = true;
 let pen = document.querySelector('.pen');
-let penToolBox = document.querySelector('.penToolBox')
+let penToolBox = document.querySelector('.penTool-Box')
 pen.addEventListener('click',()=>{
 
     toolPenFlag ? toolPenFlag = false: toolPenFlag = true;
@@ -27,6 +27,26 @@ pen.addEventListener('click',()=>{
         
     }else if(!toolPenFlag){
         penToolBox.style.display  = "";
+       
+    }
+
+    
+
+})
+
+
+
+let toolEraserBox = true;
+let eraser = document.querySelector('.eraser');
+let eraserToolBox = document.querySelector('.eraser-Box')
+eraser.addEventListener('click',()=>{
+
+    toolEraserBox ? toolEraserBox = false: toolEraserBox = true;
+    if(toolEraserBox){
+        eraserToolBox.style.display  = "none";
+        
+    }else if(!toolEraserBox){
+        eraserToolBox.style.display  = "";
        
     }
 
